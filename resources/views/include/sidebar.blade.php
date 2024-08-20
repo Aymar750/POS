8eee26c5f3a1b3a3e05260b1f051e452845a9d0d
Michele Allou <ykouadioyannevrard45@gmail.com>
2024-08-20 02:42:38 +0000

    acces utilisateur part1

--- resources/views/include/sidebar.blade.php
+++ resources/views/include/sidebar.blade.php
@@ -1,8 +1,8 @@
 <div class="app-sidebar colored">
     <div class="sidebar-header">
-        <a class="header-brand" href="{{route('dashboard')}}">
+        <a class="header-brand" href="{{ route('dashboard') }}">
             <div class="logo-img">
-               <img height="30" src="{{ asset('img/logo_white.png')}}" class="header-brand-img" title="RADMIN"> 
+                <img height="30" src="{{ asset('img/logo_white.png') }}" class="header-brand-img" title="RADMIN"> 
             </div>
         </a>
         <div class="sidebar-action"><i class="ik ik-arrow-left-circle"></i></div>
@@ -11,51 +11,54 @@
 
     @php
         $segment1 = request()->segment(1);
-        $segment2 = request()->segment(2);
+        $user = Auth::user();
+        $userRole = $user->roles->first()->name ?? ''; // Utilisation de la première relation de rôle ou une chaîne vide
     @endphp
     
     <div class="sidebar-content">
         <div class="nav-container">
             <nav id="main-menu-navigation" class="navigation-main">
                 <div class="nav-item {{ ($segment1 == 'dashboard') ? 'active' : '' }}">
-                    <a href="{{route('dashboard')}}"><i class="ik ik-bar-chart-2"></i><span>{{ __('Tableau de bord')}}</span></a>
+                    <a href="{{ route('dashboard') }}"><i class="ik ik-bar-chart-2"></i><span>{{ __('Tableau de bord') }}</span></a>
                 </div>
-                {{-- <div class="nav-lavel">{{ __('Layouts')}} </div> --}}
+
+                @if($userRole == 'Super Admin')
+                <div class="nav-item {{ ($segment1 == 'inventory') ? 'active' : '' }}">
+                    <a href="{{ url('inventory') }}"><i class="ik ik-shopping-cart"></i><span>{{ __('Inventaire') }}</span></a>
+                </div>
                 <div class="nav-item {{ ($segment1 == 'pos') ? 'active' : '' }}">
-                    <a href="{{url('inventory')}}"><i class="ik ik-shopping-cart"></i><span>{{ __('Inventaire')}}</span> </a>
+                    <a href="{{ url('pos') }}"><i class="ik ik-printer"></i><span>{{ __('POS') }}</span></a>
                 </div>
-                <div class="nav-item {{ ($segment1 == 'pos') ? 'active' : '' }}">
-                    <a href="{{url('pos')}}"><i class="ik ik-printer"></i><span>{{ __('POS')}}</span> </a>
-                </div>
-                {{-- <div class="nav-item {{ ($segment1 == 'accounting') ? 'active' : '' }}">
-                    <a href="{{url('accounting')}}"><i class="ik ik-printer"></i><span>{{ __('Accounting')}}</span> <span class=" badge badge-success badge-right">{{ __('New')}}</span></a>
-                </div> --}}
-                <div class="nav-item {{ ($segment1 == 'users' || $segment1 == 'roles'||$segment1 == 'permission' ||$segment1 == 'user') ? 'active open' : '' }} has-sub">
-                    <a href="#"><i class="ik ik-user"></i><span>{{ __('Adminstration')}}</span></a>
+                <div class="nav-item {{ ($segment1 == 'users' || $segment1 == 'roles' || $segment1 == 'permission') ? 'active open' : '' }} has-sub">
+                    <a href="#"><i class="ik ik-user"></i><span>{{ __('Administration') }}</span></a>
                     <div class="submenu-content">
-                        <!-- only those have manage_user permission will get access -->
                         @can('manage_user')
-                        <a href="{{url('users')}}" class="menu-item {{ ($segment1 == 'users') ? 'active' : '' }}">{{ __('Utilisateurs')}}</a>
-                        <a href="{{url('user/create')}}" class="menu-item {{ ($segment1 == 'user' && $segment2 == 'create') ? 'active' : '' }}">{{ __('Ajout Utilisateur')}}</a>
-                         @endcan
-                         <!-- only those have manage_role permission will get access -->
-                        @can('manage_roles')
-                        <a href="{{url('roles')}}" class="menu-item {{ ($segment1 == 'roles') ? 'active' : '' }}">{{ __('Roles')}}</a>
+                        <a href="{{ url('users') }}" class="menu-item {{ ($segment1 == 'users') ? 'active' : '' }}">{{ __('Utilisateurs') }}</a>
+                        <a href="{{ url('user/create') }}" class="menu-item {{ ($segment1 == 'user' && $segment2 == 'create') ? 'active' : '' }}">{{ __('Ajout Utilisateur') }}</a>
                         @endcan
-                        <!-- only those have manage_permission permission will get access -->
+                        @can('manage_roles')
+                        <a href="{{ url('roles') }}" class="menu-item {{ ($segment1 == 'roles') ? 'active' : '' }}">{{ __('Roles') }}</a>
+                        @endcan
                         @can('manage_permission')
-                        <a href="{{url('permission')}}" class="menu-item {{ ($segment1 == 'permission') ? 'active' : '' }}">{{ __('Permissions')}}</a>
+                        <a href="{{ url('permission') }}" class="menu-item {{ ($segment1 == 'permission') ? 'active' : '' }}">{{ __('Permissions') }}</a>
                         @endcan
                     </div>
                 </div>
-
-{{-- 
-                <!-- Include demo pages inside sidebar start-->
-                @include('pages.sidebar-menu')
-                <!-- Include demo pages inside sidebar end--> --}}
-
+                @elseif($userRole == 'Gestionnaire')
+                <div class="nav-item {{ ($segment1 == 'inventory') ? 'active' : '' }}">
+                    <a href="{{ url('inventory') }}"><i class="ik ik-shopping-cart"></i><span>{{ __('Inventaire') }}</span></a>
+                </div>
+                @elseif($userRole == 'Vendeur')
+                <div class="nav-item {{ ($segment1 == 'pos') ? 'active' : '' }}">
+                    <a href="{{ url('pos') }}"><i class="ik ik-printer"></i><span>{{ __('POS') }}</span></a>
+                </div>
+                @endif
             </nav>   
-                
         </div>
     </div>
 </div>
+
+ {{-- 
+                <!-- Include demo pages inside sidebar start-->
+                @include('pages.sidebar-menu')
+                <!-- Include demo pages inside sidebar end--> --}}
